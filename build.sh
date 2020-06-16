67f39c2b9f4ff82e7cca7a7e9455accaa7a9692e
shangjinlong <shangjinlong@unisound.com>
2020-06-16 15:48:56 +0800

    Format code

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+curPath=$(readlink -f "$(dirname "$0")")
+
 cd build
 rm -rf *
-cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain.cmake .. -DX86=ON -DUNI_VOICE_SDK_3RD_PORTAUDIO=/media/psf/Home/Desktop/workspace/github/junlon2006/voice-sdk/3rd/portaudio
+cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain.cmake .. -DX86=ON -DUNI_VOICE_SDK_3RD_PORTAUDIO=$curPath/3rd/portaudio
 make -j8
