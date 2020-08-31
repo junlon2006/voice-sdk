5d702d999de0f7a1e87ef01d9bc290df68890896
shangjinlong <shangjinlong@unisound.com>
2020-08-31 10:14:16 +0800

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 curPath=$(readlink -f "$(dirname "$0")")
 
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+
 cd build
 rm -rf *
 cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain.cmake .. -DX86=ON -DUNI_VOICE_SDK_3RD_PORTAUDIO=$curPath/3rd/portaudio
-make -j8
+make -j4
